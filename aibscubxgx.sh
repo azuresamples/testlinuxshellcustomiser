39cb3606511eaca4cb578f020498ed1913d73d19
azuresamples <54381444+azuresamples@users.noreply.github.com>
2020-07-06 14:29:42 +0900

    Update aibscubxgx.sh

--- aibscubxgx.sh
+++ aibscubxgx.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 
 #Azure Image Builder Shell Customiser Test
-
-#SELinux Setting
-setenforce 0
+sudo apt -y update
+sudo apt -y upgrade
 
 #TimeZone変更
-sed -i "s/UTC/Asia\/Tokyo/g" /etc/sysconfig/clock
+sudo timedatectl set-timezone Asia/Tokyo
 
 #日本語パッケージ
 sudo apt -y install language-pack-ja-base language-pack-ja ibus-mozc
 
 #システムの文字セットを日本語に変更
 sudo localectl set-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
-sudo source /etc/default/locale
+source /etc/default/locale
 
 #apt-getのnginx用レポジトリの作成
 cd /etc/apt/sources.list.d
 sudo touch nginx.list
 
 #ファイルのグループの変更と権限の変更
-sudo chgrp infrateam nginx.list
+#sudo chgrp infrateam nginx.list
 sudo chmod g+wr nginx.list
+sudo chmod +wr nginx.list
 
 #nginxのレポジトリ追加
-echo deb http://nginx.org/packages/ubuntu/ bionic nginx >> nginx.list
-echo deb-src http://nginx.org/packages/ubuntu/ bionic nginx >> nginx.list
+sudo sh -c "echo deb http://nginx.org/packages/ubuntu/ bionic nginx >> nginx.list"
+sudo sh -c "echo deb-src http://nginx.org/packages/ubuntu/ bionic nginx >> nginx.list"
 
 cd
 
@@ -34,12 +34,13 @@ curl http://nginx.org/keys/nginx_signing.key | sudo apt-key add -
 
 #nginxのパッケージ取得
 sudo apt -y update
+sudo apt -y upgrade
 
 #nginxのインストール
 sudo apt -y install nginx
 
-systemctl enable nginx
+sudo systemctl enable nginx
 
-systemctl start nginx
+sudo systemctl start nginx
 
 exit
